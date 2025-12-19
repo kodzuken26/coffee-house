d047e5bf9c8c5925d2dc96a866c86a78ed18cb1c
Kristina Borisova <kodzuken26@yandex.ru>
2025-12-19 14:22:21 +0400

    main готов

--- frontend/frontend1/src/App.js
+++ frontend/frontend1/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-import Header from './components/header/Header'
-
+import Header from './components/header/Header.jsx';
+import Main from './components/main/Main.jsx';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     return (
         <div>
             <Header/>
-            
+            <Main/>
             
     </div >
   );
--- frontend/frontend1/src/components/main/Main.jsx
+++ frontend/frontend1/src/components/main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./main.scss";
+import { Link } from 'react-router-dom';
 
 
 
@@ -7,8 +8,13 @@ export default function Main() {
      
     
     return (
-        <div>
-            
+        <div className="main-container">
+            <div className="main-text">
+                <h1>Кофейня “Catpuccino”</h1>
+                <p>Радуем вас свежим кофе с 2005 года!</p>
+                <button className="btn-main"><Link className="link2">Наши напитки</Link></button>
+            </div>  
+            <img src="./img/maincat.png" alt="cat-coffee" className="img-main"/>
         </div>
     )
 }
